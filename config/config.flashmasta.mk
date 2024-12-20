0e0e05eb48e1bc62d1f92a3a04818e540638dba3
Adrian Siekierka <kontakt@asie.pl>
2024-12-20 17:02:35 +0100

    config: add configurations

---
+++ config/config.flashmasta.mk
@@ -0,0 +1,6 @@
+# Flash Masta build configuration file
+
+BIOS_BANK_MEMORY := js28f00am29ew
+BIOS_BANK_ROM_FORCE_COUNT := 8
+BIOS_TIMER_RTC := none
+FLAVOR := flashmasta
---
+++ config/config.rom.mk
@@ -0,0 +1,8 @@
+# ROMable build configuration file
+# Note that this won't work with FreyaOS unless SRAM is pre-initialized,
+# as it performs a flash write self-test on boot.
+
+BIOS_BANK_MEMORY := rom
+BIOS_BANK_ROM_FORCE_COUNT := 8
+BIOS_TIMER_RTC := none
+FLAVOR := rom
