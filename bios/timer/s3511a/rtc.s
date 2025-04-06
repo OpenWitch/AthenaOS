9f554873b8b0c2ec329ac0f97427a17f914bac59
Adrian Siekierka <kontakt@asie.pl>
2025-04-06 20:58:29 +0200

    bios: s3511a: fix implementation

--- bios/timer/s3511a/rtc.s
+++ bios/timer/s3511a/rtc.s
@@ -26,34 +26,42 @@
 
 #include "common.inc"
 
-__rtc_read_byte:
+__rtc_wait_ready:
 1:
     in al, IO_CART_RTC_CTRL
+    test al, (CART_RTC_READY | CART_RTC_ACTIVE)
+    jz 2f
     test al, CART_RTC_READY
     jz 1b
+2:
+    ret
+
+__rtc_read_byte:
+    call __rtc_wait_ready
     in al, IO_CART_RTC_DATA
     ret
 
 __rtc_write_byte:
     push ax
-1:
-    in al, IO_CART_RTC_CTRL
-    test al, CART_RTC_READY
-    jz 1b
+    call __rtc_wait_ready
     pop ax
     out IO_CART_RTC_DATA, al
     ret
     
-__rtc_write_ctrl:
+__rtc_read_start:
     push ax
-1:
-    in al, IO_CART_RTC_CTRL
-    test al, CART_RTC_READY
-    jz 1b
+    call __rtc_wait_ready
     pop ax
     out IO_CART_RTC_CTRL, al
     ret
 
+__rtc_write_start:
+    push ax
+    call __rtc_wait_ready
+    pop ax
+    out IO_CART_RTC_CTRL, ax
+    ret
+    
 /**
  * INT 16h AH=03h - rtc_set_datetime_struct
  * Input:
@@ -65,10 +73,13 @@ rtc_set_datetime_struct:
     push si
 
     // Write to RTC
+    lodsb
+    call __bin_to_bcd_al
+    mov ah, al
     mov al, 0x14 // Write date/time
-    call __rtc_write_ctrl
+    call __rtc_write_start
     mov si, dx
-    mov cx, 7
+    mov cx, 6
 1:
     lodsb
     call __bin_to_bcd_al
@@ -94,7 +105,7 @@ rtc_get_datetime_struct:
 
     // Read from RTC
     mov al, 0x15 // Read date/time
-    call __rtc_write_ctrl
+    call __rtc_read_start
     mov di, dx
     mov cx, 7
 1:
@@ -119,23 +130,21 @@ rtc_get_datetime_struct:
     .global rtc_enable_alarm
 rtc_enable_alarm:
     push ax
-    // Write alarm time
-    mov al, 0x18
-    call __rtc_write_ctrl
     // Write alarm hour
     mov al, bl
     call __bin_to_bcd_al
     and al, 0x3F
-    call __rtc_write_byte
+    mov ah, al
+    // Write alarm time
+    mov al, 0x18
+    call __rtc_write_start
     // Write alarm minute
     mov al, bh
     call __bin_to_bcd_al
     call __rtc_write_byte
     // Write status
-    mov al, 0x12
-    call __rtc_write_ctrl
-    mov al, (RTC_STATUS_INT_ALARM | RTC_STATUS_24_HOUR)
-    call __rtc_write_byte
+    mov ax, ((RTC_STATUS_INT_ALARM | RTC_STATUS_24_HOUR) << 8) | 0x12
+    call __rtc_write_start
     pop ax
     ret
 
@@ -144,7 +153,7 @@ rtc_init:
     push ax
     // Read status
     mov al, 0x13
-    call __rtc_write_ctrl
+    call __rtc_read_start
     call __rtc_read_byte
 
     // Was power loss detected?
@@ -162,9 +171,10 @@ rtc_reset:
     push ax
 1:
     // Reset
-    mov al, 0x10
-    call __rtc_write_ctrl
+    mov ax, 0xFF10
+    call __rtc_write_start
     pop ax
+
 /**
  * INT 16h AH=06h - rtc_disable_alarm
  * Input:
@@ -174,9 +184,7 @@ rtc_reset:
 rtc_disable_alarm:
     push ax
     // Write status
-    mov al, 0x12
-    call __rtc_write_ctrl
-    mov al, (RTC_STATUS_24_HOUR)
-    call __rtc_write_byte
+    mov ax, ((RTC_STATUS_24_HOUR) << 8) | 0x12
+    call __rtc_write_start
     pop ax
     ret
