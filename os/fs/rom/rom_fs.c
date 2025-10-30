94c3605c276d9f9d678dd9e2e9567a89d7170145
Adrian Siekierka <kontakt@asie.pl>
2025-10-30 21:01:43 +0100

    os/fs/rom: implement basic read-only file I/O

--- os/fs/rom/rom_fs.c
+++ os/fs/rom/rom_fs.c
@@ -20,12 +20,15 @@
  * SOFTWARE.
  */
 
+#include <stdio.h>
 #include <string.h>
 #include <wonderful.h>
 #include "common.h"
 #include "sys/bios.h"
 #include "fs/kern_fs.h"
 #include "rom_fs.h"
+
+#define fhandle(fd) (sramwork_p->_openfiles[(fd)])
 
 IL_FUNCTION
 fent_t __far* __far fs_entries(FS fs) {
@@ -75,14 +78,13 @@ static fent_t __far* find_fs_entry(FS fs, const char __seg_ss* filename) {
 IL_FUNCTION
 int fs_getent(FS fs, int index, fent_t __far* entry) {
     fent_t buffer;
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    if (index < 0 || index >= fs_n_entries(fs)) {
-        ws_bank_ram_restore(bank);
-        return E_FS_OUT_OF_BOUNDS;
-    }
-    fent_t __far* files = fs_entries(fs);
-    memcpy(&buffer, files + index, sizeof(fent_t));
-    ws_bank_ram_restore(bank);
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (index < 0 || index >= fs_n_entries(fs)) {
+            return E_FS_OUT_OF_BOUNDS;
+        }
+        fent_t __far* files = fs_entries(fs);
+        memcpy(&buffer, files + index, sizeof(fent_t));
+    });
     memcpy(entry, &buffer, sizeof(fent_t));
     return 0;
 }
@@ -94,14 +96,13 @@ int fs_findent(FS fs, const char __far* filename, fent_t __far* entry) {
     strncpy(filename_local, filename, MAXFNAME);
 
     fent_t buffer;
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    fent_t __far* src_entry = find_fs_entry(fs, filename_local);
-    if (src_entry == NULL) {
-        ws_bank_ram_restore(bank);
-        return E_FS_FILE_NOT_FOUND;
-    }
-    memcpy(&buffer, src_entry, sizeof(fent_t));
-    ws_bank_ram_restore(bank);
+    ws_bank_with_ram(BANK_OSWORK, {
+        fent_t __far* src_entry = find_fs_entry(fs, filename_local);
+        if (src_entry == NULL) {
+            return E_FS_FILE_NOT_FOUND;
+        }
+        memcpy(&buffer, src_entry, sizeof(fent_t));
+    });
     memcpy(entry, &buffer, sizeof(fent_t));
     return 0;
 }
@@ -112,33 +113,78 @@ void __far * __far fs_mmap(FS fs, const char __far *filename) {
     strncpy(filename_local, filename, MAXFNAME);
 
     void __far *result = NULL;
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    if (!(fs->mode & FMODE_MMAP)) {
-        fent_t __far* src_entry = find_fs_entry(fs, filename_local);
-        if (src_entry != NULL) {
-            result = src_entry->loc;
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (!(fs->mode & FMODE_MMAP)) {
+            fent_t __far* src_entry = find_fs_entry(fs, filename_local);
+            if (src_entry != NULL) {
+                result = src_entry->loc;
+            }
         }
-    }
-    ws_bank_ram_restore(bank);
+    });
     return result;
 }
 
 IL_FUNCTION
 int fs_open(FS fs, const char __far *filename, int mode, int perms) {
-    // TODO
-    return E_FS_ERROR;
+    char filename_local[MAXFNAME];
+    strncpy(filename_local, filename, MAXFNAME);
+
+    ws_bank_with_ram(BANK_OSWORK, {
+        int free_fd;
+        for (free_fd = 0; free_fd < MAXFILES; free_fd++)
+            if (fhandle(free_fd).ent == NULL) break;
+        if (free_fd == MAXFILES)
+            return E_FS_OUT_OF_BOUNDS;
+    
+        fent_t __far* src_entry = find_fs_entry(fs, filename_local);
+        if (src_entry == NULL)
+            return E_FS_FILE_NOT_FOUND;
+
+        fhandle_t *h = &fhandle(free_fd);
+        h->fs = fs;
+        h->ent = src_entry;
+        h->mode = mode;
+        h->loc.fp = src_entry->loc;
+        h->len = src_entry->len;
+        h->count = src_entry->count;
+        h->pos = 0;
+        
+        return E_FS_SUCCESS;
+    });
 }
 
 IL_FUNCTION
 int fs_close(int fd) {
-    // TODO
-    return E_FS_ERROR;
+    if (fd < 0 || fd >= MAXFILES)
+        return E_FS_OUT_OF_BOUNDS;
+
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (fhandle(fd).ent == NULL)
+            return E_FS_ERROR;
+        fhandle(fd).ent = NULL;
+        return E_FS_SUCCESS;
+    });
 }
 
 IL_FUNCTION
 int fs_read(int fd, char __far *data, int length) {
-    // TODO
-    return E_FS_ERROR;
+    if (fd < 0 || fd >= MAXFILES)
+        return E_FS_OUT_OF_BOUNDS;
+
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (fhandle(fd).ent == NULL)
+            return E_FS_ERROR;
+
+        fpos_t pos = fhandle(fd).pos;
+        const void __far* src = MK_FP(fhandle(fd).loc.w.seg + (pos >> 4), pos & 0xF);
+        int to_read = length;
+        if (to_read > (fhandle(fd).len - pos))
+            to_read = fhandle(fd).len - pos;
+
+        memcpy(data, src, to_read);
+        fhandle(fd).pos += to_read;
+        return to_read;
+    });
 }
 
 IL_FUNCTION
@@ -149,8 +195,28 @@ int fs_write(int fd, const char __far *data, int length) {
 
 IL_FUNCTION
 int fs_lseek(int fd, long offset, int whence) {
-    // TODO
-    return E_FS_ERROR;
+    if (fd < 0 || fd >= MAXFILES)
+        return E_FS_OUT_OF_BOUNDS;
+    if (whence < 0 || whence > 2)
+        return E_FS_ERROR;
+    
+    ws_bank_with_ram(BANK_OSWORK, {
+        fpos_t new_pos;
+        if (whence == SEEK_SET)
+            new_pos = offset;
+        else if (whence == SEEK_CUR)
+            new_pos = fhandle(fd).pos + offset;
+        else if (whence == SEEK_END)
+            new_pos = fhandle(fd).len + offset;
+
+        if (new_pos < 0)
+            new_pos = 0;
+        else if (new_pos > fhandle(fd).len)
+            new_pos = fhandle(fd).len;
+
+        fhandle(fd).pos = new_pos;
+        return E_FS_SUCCESS;
+    });
 }
 
 IL_FUNCTION
@@ -158,44 +224,42 @@ int fs_chmod(FS fs, const char __far *filename, int mode) {
     char filename_local[MAXFNAME];
     strncpy(filename_local, filename, MAXFNAME);
 
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    if (!(fs->mode & FMODE_W)) {
-        ws_bank_ram_restore(bank);
-        return E_FS_PERMISSION_DENIED;
-    }
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (!(fs->mode & FMODE_W)) {
+            return E_FS_PERMISSION_DENIED;
+        }
 
-    fent_t __far* src_entry = find_fs_entry(fs, filename_local);
-    if (src_entry == NULL) {
-        ws_bank_ram_restore(bank);
-        return E_FS_FILE_NOT_FOUND;
-    }
+        fent_t __far* src_entry = find_fs_entry(fs, filename_local);
+        if (src_entry == NULL) {
+            return E_FS_FILE_NOT_FOUND;
+        }
 
-    src_entry->mode = mode;
-    ws_bank_ram_restore(bank);
-    return E_FS_SUCCESS;
+        src_entry->mode = mode;
+        return E_FS_SUCCESS;
+    });
 }
 
 IL_FUNCTION
 int fs_creat(FS fs, fent_t __far *entry) {
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    if (!(fs->mode & FMODE_W)) {
-        ws_bank_ram_restore(bank);
-        return E_FS_PERMISSION_DENIED;
-    }
-
-    int n = fs_n_entries(fs);
-    fent_t __far* files = fs_entries(fs);
-
-    for (int i = 0; i < n; i++) {
-        if (files[i].count < 0) {
-            memcpy(files + i, entry, sizeof(fent_t));
-            ws_bank_ram_restore(bank);
-            return E_FS_SUCCESS;
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (!(fs->mode & FMODE_W)) {
+            return E_FS_PERMISSION_DENIED;
         }
-    }
 
-    ws_bank_ram_restore(bank);
-    return E_FS_NO_SPACE_LEFT;
+        int n = fs_n_entries(fs);
+        fent_t __far* files = fs_entries(fs);
+
+        for (int i = 0; i < n; i++) {
+            if (files[i].count < 0) {
+                memcpy(files + i, entry, sizeof(fent_t));
+
+                // TODO: allocate room for file
+                return E_FS_ERROR;
+            }
+        }
+
+        return E_FS_NO_SPACE_LEFT;
+    });
 }
 
 IL_FUNCTION
@@ -203,41 +267,39 @@ int fs_unlink(FS fs, const char __far *filename) {
     char filename_local[MAXFNAME];
     strncpy(filename_local, filename, MAXFNAME);
 
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    if (!(fs->mode & FMODE_W)) {
-        ws_bank_ram_restore(bank);
-        return E_FS_PERMISSION_DENIED;
-    }
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (!(fs->mode & FMODE_W)) {
+            return E_FS_PERMISSION_DENIED;
+        }
 
-    fent_t __far* src_entry = find_fs_entry(fs, filename_local);
-    if (src_entry == NULL) {
-        ws_bank_ram_restore(bank);
-        return E_FS_FILE_NOT_FOUND;
-    }
+        fent_t __far* src_entry = find_fs_entry(fs, filename_local);
+        if (src_entry == NULL) {
+            return E_FS_FILE_NOT_FOUND;
+        }
 
-    src_entry->count = -1;
-    src_entry->resource = -1;
-    ws_bank_ram_restore(bank);
+        src_entry->count = -1;
+        src_entry->resource = -1;
+    });
+
     return E_FS_SUCCESS;
 }
 
 IL_FUNCTION
 int fs_newfs(FS fs) {
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
+    ws_bank_with_ram(BANK_OSWORK, {
+        int n = fs_n_entries(fs);
+        fent_t __far* files = fs_entries(fs);
 
-    int n = fs_n_entries(fs);
-    fent_t __far* files = fs_entries(fs);
+        memset(files, 0, sizeof(fent_t) * n);
+        for (int i = 0; i < n; i++) {
+            files[i].count = -1;
+            files[i].resource = -1;
+        }
 
-    memset(files, 0, sizeof(fent_t) * n);
-    for (int i = 0; i < n; i++) {
-        files[i].count = -1;
-        files[i].resource = -1;
-    }
+        fs->il = il_fs_ptr();
+        fs->count = n;
+    });
 
-    fs->il = il_fs_ptr();
-    fs->count = n;
-
-    ws_bank_ram_restore(bank);
     return E_FS_SUCCESS;
 }
 
@@ -249,29 +311,28 @@ int fs_defrag(FS fs) {
 
 IL_FUNCTION
 uint32_t fs_space(FS fs) {
-    volatile uint16_t bank = ws_bank_ram_save(BANK_OSWORK);
-    if (!(fs->mode & FMODE_W)) {
-        ws_bank_ram_restore(bank);
-        return 0;
-    }
+    ws_bank_with_ram(BANK_OSWORK, {
+        if (!(fs->mode & FMODE_W)) {
+            return 0;
+        }
 
-    int n = fs_n_entries(fs);
-    fent_t __far* files = fs_entries(fs);
+        int n = fs_n_entries(fs);
+        fent_t __far* files = fs_entries(fs);
 
-    uint32_t max_file_end = ws_ptr_to_linear(fs->loc);
-    uint32_t space_end = max_file_end + fs->len;
+        uint32_t max_file_end = ws_ptr_to_linear(fs->loc);
+        uint32_t space_end = max_file_end + fs->len;
 
-    for (int i = 0; i < n; i++) {
-        if (files[i].count >= 0) {
-            uint32_t file_end = ws_ptr_to_linear(files[i].loc) + ((files[i].len + 15) & ~15);
-            if (file_end > max_file_end) {
-                max_file_end = file_end;
+        for (int i = 0; i < n; i++) {
+            if (files[i].count >= 0) {
+                uint32_t file_end = ws_ptr_to_linear(files[i].loc) + ((files[i].len + 15) & ~15);
+                if (file_end > max_file_end) {
+                    max_file_end = file_end;
+                }
             }
         }
-    }
 
-    ws_bank_ram_restore(bank);
-    return space_end - max_file_end;
+        return space_end - max_file_end;
+    });
 }
 
 // FIXME
@@ -310,6 +371,8 @@ const fent_t *fs_init(void) {
     strcpy(ram0_fs->name, "ram0");
     strcpy(rom0_fs->name, "rom0");
     strcpy(kern_fs->name, "kern");
+
+    memset(sramwork_p->_openfiles, 0, sizeof(sramwork_p->_openfiles));
 
     return rom0_fs_entries + rom_fs_footer->rom0_executable_idx;
 }
